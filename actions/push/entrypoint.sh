8a2c66f645fb2b0a9bce43a3d0184fc2bf3158af
Christoph Schweppe <info@cschweppe.de>
2019-02-20 10:12:56 +0100

    Remove old action

--- actions/push/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-git init
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-
-git subtree push --prefix build_production origin gh-pages
